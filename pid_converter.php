103383d974b02ade687e9357538549301e38dc13
UtopiaOpera <utopiaopera@gmail.com>
2019-11-21 22:06:19 +0000

    Update pid_converter.php

--- pid_converter.php
+++ pid_converter.php
@@ -5,7 +5,7 @@
 // 1a. Generate Task List as CSV file: Run CMD as Admin and run command  tasklist/svc /FO json > c:\processes_list.json
 // 1b. You can change the file location in the above command to wherever. 
 // 2.  Convert the CSV file to JSON at: https://www.csvjson.com/csv2json
-// 3.  Place this file (gm_pid_converter.php) and the converted JSON file (file name MUST be csvjson.json) into the same
+// 3.  Place this file (pid_converter.php) and the converted JSON file (file name MUST be csvjson.json) into the same
 //     folder on your server.
 // 4.  Go to file on server and see the output.
 
